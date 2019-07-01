a595cb6dcdd90df4e6bd92c742440378f0f117f4
heathmcgowan <heathjmcgowan@gmail.com>
2019-07-01 18:52:25 +1200

    major update. Added contact form, portfolio projects and other design changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,46 @@
-let currentPage = 0;
+const portfolio = document.getElementById("portfolio");
 
 $(document).ready(function() {
-        
+    $('.nav-btn').click(function () {
+        removeActiveNav();
+        addActiveNav(this);
+        if ($(this).hasClass('home-btn') && $('.contact-modal').hasClass('onscreen')) {
+            hideContactModal();
+        };
+        if ($(this).hasClass('portfolio-btn')) {
+            hideContactModal();
+            portfolio.scrollIntoView();
+        };
+        if ($(this).hasClass('contact-btn') && $('.contact-modal').hasClass('offscreen')) {
+            showContactModal();
+        };
+    });
+
+    $('.fa-caret-up').click(function() {
+        removeActiveNav();
+        $('.home-btn').addClass('active-nav-item');
+        hideContactModal();
+    });
+
+    function removeActiveNav() {
+        $('.nav-btn').removeClass('active-nav-item');
+    }
+    
+    function addActiveNav(x) {
+        $(x).removeClass('non-active-nav-item');
+        $(x).addClass('active-nav-item');
+    }
+
+    function showContactModal() {
+        $('.contact-modal').removeClass('offscreen');
+        $('.contact-modal').addClass('onscreen');
+        $('.about-content').addClass('hidden-opacity');
+    }
+
+    function hideContactModal() {
+        $('.contact-modal').addClass('offscreen');
+        $('.about-content').removeClass('hidden-opacity');
+    }
+    
 });
+
